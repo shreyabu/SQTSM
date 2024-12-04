1aea32d6dc9ba0fe09a2cd0d9d36d6193c32d81d
LeiZ54 <zlei9549@gmail.com>
2024-12-04 15:28:48 -0500

    update propertoes

--- src/main/java/org/lei/personalized_advertisement_system/config/CorsConfig.java
+++ src/main/java/org/lei/personalized_advertisement_system/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package org.lei.personalized_advertisement_system.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -8,13 +9,14 @@ import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class CorsConfig {
+    @Value("${frontend.allowed-origin}")
+    private String allowedOrigin;
 
     @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("http://3.133.115.55");
-//        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedOrigin(allowedOrigin);
         config.addAllowedMethod("*");
         config.addAllowedHeader("*");
         config.setAllowCredentials(true); // 如果需要支持 Cookie
